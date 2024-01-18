3f6b922c031f9b31652c3998f0ce1e985629c62a
Sam Marks <saprmarks@gmail.com>
2024-01-18 17:51:13 -0500

    typo fix

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-def hf_dataset_to_iterator(dataset_name, split='train', streaming=True):
+def hf_dataset_to_generator(dataset_name, split='train', streaming=True):
     dataset = load_dataset(dataset_name, split=split, streaming=streaming)
     
     def gen():
