82fff1968ae883afec82d14246041df793ffd170
Sam Marks <saprmarks@gmail.com>
2024-01-18 17:49:50 -0500

    added utils for converting hf dataset to generator

---
+++ utils.py
@@ -0,0 +1,10 @@
+from datasets import load_dataset
+
+def hf_dataset_to_iterator(dataset_name, split='train', streaming=True):
+    dataset = load_dataset(dataset_name, split=split, streaming=streaming)
+    
+    def gen():
+        for x in iter(dataset):
+            yield x['text']
+    
+    return gen()
