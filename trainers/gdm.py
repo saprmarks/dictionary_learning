3567d6d2a2cb6d810df32b838029daacc354aaaa
saprmarks <saprmarks@gmail.com>
2024-04-26 18:31:16 -0400

    gatedSAE trainer

---
+++ trainers/gdm.py
@@ -0,0 +1,102 @@
+"""
+Implements the training scheme for a gated SAE described in https://arxiv.org/abs/2404.16014
+"""
+
+import torch as t
+from ..trainers.trainer import SAETrainer
+from ..config import DEBUG
+from ..dictionary import GatedAutoEncoder
+
+class ConstrainedAdam(t.optim.Adam):
+    """
+    A variant of Adam where some of the parameters are constrained to have unit norm.
+    """
+    def __init__(self, params, constrained_params, lr):
+        super().__init__(params, lr=lr, betas=(0, 0.999))
+        self.constrained_params = list(constrained_params)
+    
+    def step(self, closure=None):
+        with t.no_grad():
+            for p in self.constrained_params:
+                normed_p = p / p.norm(dim=0, keepdim=True)
+                # project away the parallel component of the gradient
+                p.grad -= (p.grad * normed_p).sum(dim=0, keepdim=True) * normed_p
+        super().step(closure=closure)
+        with t.no_grad():
+            for p in self.constrained_params:
+                # renormalize the constrained parameters
+                p /= p.norm(dim=0, keepdim=True)
+
+class GatedSAETrainer(SAETrainer):
+    """
+    Gated SAE training scheme.
+    """
+    def __init__(self,
+                 dict_class=GatedAutoEncoder,
+                 activation_dim=512,
+                 dict_size=64*512,
+                 lr=3e-4, 
+                 l1_penalty=1e-1,
+                 warmup_steps=1000, # lr warmup period at start of training and after each resample
+                 resample_steps=None, # how often to resample neurons
+                 seed=None,
+                 device=None,
+    ):
+        super().__init__(seed)
+
+        if seed is not None:
+            t.manual_seed(seed)
+            t.cuda.manual_seed_all(seed)
+
+        # initialize dictionary
+        self.ae = dict_class(activation_dim, dict_size)
+
+        self.lr = lr
+        self.l1_penalty=l1_penalty
+        self.warmup_steps = warmup_steps
+
+        if device is None:
+            self.device = 'cuda' if t.cuda.is_available() else 'cpu'
+        else:
+            self.device = device
+        self.ae.to(self.device)
+
+        self.optimizer = ConstrainedAdam(
+            self.ae.parameters(),
+            self.ae.decoder.parameters(),
+            lr=lr
+        )
+        def warmup_fn(step):
+            return min(1, step / warmup_steps)
+        self.scheduler = t.optim.lr_scheduler.LambdaLR(self.optimizer, warmup_fn)
+
+    def loss(self, x):
+        x_hat = self.ae(x)
+        f_gate = self.ae.encode(x, gate_only=True)
+        x_hat_gate = f_gate @ self.ae.decoder.weight.detach().T + self.ae.decoder_bias.detach()
+
+        L_recon = (x - x_hat).pow(2).sum(dim=-1).mean()
+        L_sparse = t.linalg.norm(f_gate, ord=1, dim=-1).mean()
+        L_aux = (x - x_hat_gate).pow(2).sum(dim=-1).mean()
+
+        return L_recon + self.l1_penalty * L_sparse + L_aux
+    
+    def update(self, step, x):
+        x = x.to(self.device)
+        self.optimizer.zero_grad()
+        loss = self.loss(x)
+        loss.backward()
+        self.optimizer.step()
+        self.scheduler.step()
+
+    @property
+    def config(self):
+        return {
+            'trainer_class' : 'GatedSAETrainer',
+            'activation_dim' : self.ae.activation_dim,
+            'dict_size' : self.ae.dict_size,
+            'lr' : self.lr,
+            'l1_penalty' : self.l1_penalty,
+            'warmup_steps' : self.warmup_steps,
+            'device' : self.device,
+        }
