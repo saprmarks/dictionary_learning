09dacde50e93e7bb2161819b9b914e4cbe73c9be
Rico Angell <rangell@cs.umass.edu>
2024-04-29 11:02:13 -0400

    .

--- grad_pursuit.py
+++ grad_pursuit.py
@@ -5,18 +5,18 @@ https://www.lesswrong.com/posts/C5KAZQib3bzzpeyrg/full-post-progress-update-1-fr
 
 import torch as t
 
-def _grad_pursuit_update_step(signal, weights, dictionary):
+
+
+def _grad_pursuit_update_step(signal, weights, dictionary, batch_arange, selected_features):
     """
     signal: b x d, weights: b x n, dictionary: d x n
     """
-    # get a mask for which features have already been chosen (ie have nonzero weights)
     residual = signal - t.einsum('bn,dn -> bd', weights, dictionary)
-    selected_features = (weights != 0)
-    # choose the element with largest inner product, as in matched pursuit.
+    # choose the element with largest inner product with residual, as in matched pursuit.
     inner_products = t.einsum('dn,bd -> bn', dictionary, residual)
     idxs = t.argmax(inner_products, dim=1)
     # add the new feature to the active set. 
-    selected_features[t.arange(idxs.shape[0]), idxs] = 1
+    selected_features[batch_arange, idxs] = 1
 
     # the gradient for the weights is the inner product, restricted to the chosen features
     grad = selected_features * inner_products
@@ -25,7 +25,7 @@ def _grad_pursuit_update_step(signal, weights, dictionary):
     step_size = t.einsum('bd,bd -> b', c, residual) / t.einsum('bd,bd -> b ', c, c)
     weights = weights + t.einsum('b,bn -> bn', step_size, grad)
     weights = t.clip(weights, min=0) # clip the weights to be positive
-    return weights
+    return weights, selected_features
 
 @t.jit.script
 def grad_pursuit(signal, dictionary, target_l0 : int = 20, device : str = 'cpu'):
@@ -35,7 +35,10 @@ def grad_pursuit(signal, dictionary, target_l0 : int = 20, device : str = 'cpu')
     """
     assert len(signal.shape) == 2  # makes sure this a batch of signals
     with t.no_grad():
+        batch_arange = t.arange(signal.shape[0]).to(device)
         weights = t.zeros((signal.shape[0], dictionary.shape[1])).to(device)
+        selected_features = t.zeros((signal.shape[0], dictionary.shape[1])).to(device)
         for _ in range(target_l0):
-            weights = _grad_pursuit_update_step(signal, weights, dictionary)
+            weights, selected_features = _grad_pursuit_update_step(
+                signal, weights, dictionary, batch_arange, selected_features)
     return weights
