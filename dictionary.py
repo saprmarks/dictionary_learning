254329385532dc9c21d24e76e23cd324e4ba5e80
Kola Ayonrinde <koayon@gmail.com>
2024-05-01 18:40:24 +0100

    Add types to dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,35 +3,41 @@ Defines the dictionary classes
 """
 
 from abc import ABC, abstractmethod
+from typing import Optional
+
 import torch as t
 import torch.nn as nn
+
 
 class Dictionary(ABC):
     """
     A dictionary consists of a collection of vectors, an encoder, and a decoder.
     """
-    dict_size : int # number of features in the dictionary
-    activation_dim : int # dimension of the activation vectors
+
+    dict_size: int  # number of features in the dictionary
+    activation_dim: int  # dimension of the activation vectors
 
     @abstractmethod
-    def encode(self, x):
+    def encode(self, x: t.Tensor) -> t.Tensor:
         """
         Encode a vector x in the activation space.
         """
         pass
-    
+
     @abstractmethod
-    def decode(self, f):
+    def decode(self, f: t.Tensor) -> t.Tensor:
         """
         Decode a dictionary vector f (i.e. a linear combination of dictionary elements)
         """
         pass
 
+
 class AutoEncoder(Dictionary, nn.Module):
     """
     A one-layer autoencoder.
     """
-    def __init__(self, activation_dim, dict_size):
+
+    def __init__(self, activation_dim: int, dict_size: int):
         super().__init__()
         self.activation_dim = activation_dim
         self.dict_size = dict_size
@@ -44,67 +50,74 @@ class AutoEncoder(Dictionary, nn.Module):
         dec_weight = dec_weight / dec_weight.norm(dim=0, keepdim=True)
         self.decoder.weight = nn.Parameter(dec_weight)
 
-    def encode(self, x):
+    def encode(self, x: t.Tensor) -> t.Tensor:
         return nn.ReLU()(self.encoder(x - self.bias))
-    
-    def decode(self, f):
+
+    def decode(self, f: t.Tensor) -> t.Tensor:
         return self.decoder(f) + self.bias
-    
-    def forward(self, x, output_features=False, ghost_mask=None):
+
+    def forward(
+        self, x: t.Tensor, output_features: bool = False, ghost_mask: Optional[t.Tensor] = None
+    ):
         """
         Forward pass of an autoencoder.
         x : activations to be autoencoded
         output_features : if True, return the encoded features as well as the decoded x
         ghost_mask : if not None, run this autoencoder in "ghost mode" where features are masked
         """
-        if ghost_mask is None: # normal mode
+        if ghost_mask is None:  # normal mode
             f = self.encode(x)
             x_hat = self.decode(f)
             if output_features:
                 return x_hat, f
             else:
                 return x_hat
-        
-        else: # ghost mode
+
+        else:  # ghost mode
             f_pre = self.encoder(x - self.bias)
             f_ghost = t.exp(f_pre) * ghost_mask.to(f_pre)
             f = nn.ReLU()(f_pre)
 
-            x_ghost = self.decoder(f_ghost) # note that this only applies the decoder weight matrix, no bias
+            x_ghost = self.decoder(
+                f_ghost
+            )  # note that this only applies the decoder weight matrix, no bias
             x_hat = self.decode(f)
             if output_features:
                 return x_hat, x_ghost, f
             else:
                 return x_hat, x_ghost
-            
-    def from_pretrained(path, device=None):
+
+    @classmethod
+    def from_pretrained(cls, path, device=None):
         """
         Load a pretrained autoencoder from a file.
         """
         state_dict = t.load(path)
-        dict_size, activation_dim = state_dict['encoder.weight'].shape
+        dict_size, activation_dim = state_dict["encoder.weight"].shape
         autoencoder = AutoEncoder(activation_dim, dict_size)
         autoencoder.load_state_dict(state_dict)
         if device is not None:
             autoencoder.to(device)
         return autoencoder
-            
+
+
 class IdentityDict(Dictionary, nn.Module):
     """
     An identity dictionary, i.e. the identity function.
     """
-    def __init__(self, activation_dim=None):
-        super().__init__()
-        self.activation_dim = activation_dim
-        self.dict_size = activation_dim
 
-    def encode(self, x):
+    def __init__(self, activation_dim: Optional[int] = None):
+        super().__init__()
+
+    def encode(self, x: t.Tensor) -> t.Tensor:
         return x
-    
-    def decode(self, f):
+
+    def decode(self, f: t.Tensor) -> t.Tensor:
         return f
-    
-    def forward(self, x, output_features=False, ghost_mask=None):
+
+    def forward(
+        self, x: t.Tensor, output_features: bool = False, ghost_mask: Optional[t.Tensor] = None
+    ):
         if output_features:
             return x, x
         else:
