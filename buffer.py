f44d889658ce6bdb0ef8b466677c0347bb5e4f79
Kola Ayonrinde <koayon@gmail.com>
2024-05-01 20:13:48 +0100

    Format buffer.py

--- buffer.py
+++ buffer.py
@@ -1,11 +1,12 @@
 import torch as t
 from nnsight import LanguageModel
+
 from .config import DEBUG
 
 if DEBUG:
-    tracer_kwargs = {'scan' : True, 'validate' : True}
+    tracer_kwargs = {"scan": True, "validate": True}
 else:
-    tracer_kwargs = {'scan' : False, 'validate' : False}
+    tracer_kwargs = {"scan": False, "validate": False}
 
 
 class ActivationBuffer:
@@ -13,30 +14,34 @@ class ActivationBuffer:
     Implements a buffer of activations. The buffer stores activations from a model,
     yields them in batches, and refreshes them when the buffer is less than half full.
     """
-    def __init__(self, 
-                 data, # generator which yields text data
-                 model : LanguageModel, # LanguageModel from which to extract activations
-                 submodule, # submodule of the model from which to extract activations
-                 d_submodule=None, # submodule dimension; if None, try to detect automatically
-                 io='out', # can be 'in' or 'out'; whether to extract input or output activations
-                 n_ctxs=3e4, # approximate number of contexts to store in the buffer
-                 ctx_len=128, # length of each context
-                 refresh_batch_size=512, # size of batches in which to process the data when adding to buffer
-                 out_batch_size=8192, # size of batches in which to yield activations
-                 device='cpu' # device on which to store the activations
-                 ):
-        
-        if io not in ['in', 'out']:
+
+    def __init__(
+        self,
+        data,  # generator which yields text data
+        model: LanguageModel,  # LanguageModel from which to extract activations
+        submodule,  # submodule of the model from which to extract activations
+        d_submodule=None,  # submodule dimension; if None, try to detect automatically
+        io="out",  # can be 'in' or 'out'; whether to extract input or output activations
+        n_ctxs=3e4,  # approximate number of contexts to store in the buffer
+        ctx_len=128,  # length of each context
+        refresh_batch_size=512,  # size of batches in which to process the data when adding to buffer
+        out_batch_size=8192,  # size of batches in which to yield activations
+        device="cpu",  # device on which to store the activations
+    ):
+
+        if io not in ["in", "out"]:
             raise ValueError("io must be either 'in' or 'out'")
 
         if d_submodule is None:
             try:
-                if io == 'in':
+                if io == "in":
                     d_submodule = submodule.in_features
                 else:
                     d_submodule = submodule.out_features
             except:
-                raise ValueError("d_submodule cannot be inferred and must be specified directly")
+                raise ValueError(
+                    "d_submodule cannot be inferred and must be specified directly"
+                )
         self.activations = t.empty(0, d_submodule, device=device)
 
         self.read = t.zeros(0).bool()
@@ -51,7 +56,7 @@ class ActivationBuffer:
         self.refresh_batch_size = refresh_batch_size
         self.out_batch_size = out_batch_size
         self.device = device
-    
+
     def __iter__(self):
         return self
 
@@ -66,10 +71,12 @@ class ActivationBuffer:
 
             # return a batch
             unreads = (~self.read).nonzero().squeeze()
-            idxs = unreads[t.randperm(len(unreads), device=unreads.device)[:self.out_batch_size]]
+            idxs = unreads[
+                t.randperm(len(unreads), device=unreads.device)[: self.out_batch_size]
+            ]
             self.read[idxs] = True
             return self.activations[idxs]
-    
+
     def text_batch(self, batch_size=None):
         """
         Return a list of text
@@ -77,38 +84,36 @@ class ActivationBuffer:
         if batch_size is None:
             batch_size = self.refresh_batch_size
         try:
-            return [
-                next(self.data) for _ in range(batch_size)
-            ]
+            return [next(self.data) for _ in range(batch_size)]
         except StopIteration:
             raise StopIteration("End of data stream reached")
-    
+
     def tokenized_batch(self, batch_size=None):
         """
         Return a batch of tokenized inputs.
         """
         texts = self.text_batch(batch_size=batch_size)
         return self.model.tokenizer(
-            texts,
-            return_tensors='pt',
-            max_length=self.ctx_len,
-            padding=True,
-            truncation=True
+            texts, return_tensors="pt", max_length=self.ctx_len, padding=True, truncation=True
         )
 
     def refresh(self):
         self.activations = self.activations[~self.read]
 
         while len(self.activations) < self.n_ctxs * self.ctx_len:
-            
+
             with t.no_grad():
-                with self.model.trace(self.text_batch(), **tracer_kwargs, invoker_args={'truncation': True, 'max_length': self.ctx_len}):
-                    if self.io == 'in':
+                with self.model.trace(
+                    self.text_batch(),
+                    **tracer_kwargs,
+                    invoker_args={"truncation": True, "max_length": self.ctx_len}
+                ):
+                    if self.io == "in":
                         hidden_states = self.submodule.input[0].save()
                     else:
                         hidden_states = self.submodule.output.save()
                     input = self.model.input.save()
-            attn_mask = input.value[1]['attention_mask']
+            attn_mask = input.value[1]["attention_mask"]
             hidden_states = hidden_states.value
             if isinstance(hidden_states, tuple):
                 hidden_states = hidden_states[0]
