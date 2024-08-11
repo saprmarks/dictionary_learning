d94697df1783da8b6739e565c3a1bd297b8b1e98
Adam Karvonen <adam.karvonen@gmail.com>
2024-08-11 20:56:48 +0000

    Revert to triton TopK implementation

--- trainers/top_k.py
+++ trainers/top_k.py
@@ -46,6 +46,23 @@ def geometric_median(points: t.Tensor, max_iter: int = 100, tol: float = 1e-5):
 class AutoEncoderTopK(Dictionary, nn.Module):
     """
     The top-k autoencoder architecture and initialization used in https://arxiv.org/abs/2406.04093
+    NOTE: This implementation uses OpenAI's Triton kernels for the decoder.
+    This causes two issues:
+
+    1. encode() returns topk_indices and topk_values, rather than f, because the Triton kernel requires this.
+    This breaks the abstraction we use in other dictionary classes, meaning that TopK doesn't work with
+    evaluation.py, attribution patching, etc.
+    2. The Triton kernel requires the decoder weights to be stored in the same shape as the encoder weights.
+    Thus, accessing TopK decoder vectors is dictionary.decoder[idx, :], while it's dictionary.decoder.weight[:, idx]
+    for everything else.
+
+    When benchmarking TopK training on Pythia70m with and without Triton, I did not notice any significant speedup.
+    This may be because the small size of pythia or `dictionary_learning` isn't sufficiently optimized to
+    take advantage of Triton's speedup.
+
+    `dictionary_learning` prioritizes ease of use, not efficiency. As a result, Triton will only be used in the
+    `topk-triton-implementation` branch, which will not be maintained. The main branch will use a TopK format
+    compatible with the rest of the library.
     """
 
     def __init__(self, activation_dim, dict_size, k):
@@ -62,30 +79,19 @@ class AutoEncoderTopK(Dictionary, nn.Module):
 
         self.b_dec = self.b_dec = nn.Parameter(t.zeros(activation_dim))
 
-    # def encode(self, x: t.Tensor) -> t.Tensor:
-    #     pre_relu_f = nn.functional.relu(self.encoder(x - self.b_dec))
-    #     top_acts, top_indices = pre_relu_f.topk(self.k, sorted=False, dim=-1)
-    #     buf = top_acts.new_zeros(top_acts.shape[:-1] + (self.decoder.mT.shape[-1],))
-    #     acts = buf.scatter_(dim=-1, index=top_indices, src=top_acts)
-    #     return acts
+    def encode(self, x):
+        return nn.functional.relu(self.encoder(x - self.b_dec))
 
-    def encode(self, x: t.Tensor) -> t.Tensor:
-        post_relu_f = nn.functional.relu(self.encoder(x - self.b_dec))
-        post_topk = post_relu_f.topk(self.k, sorted=False, dim=-1)
-        acts = t.zeros(post_relu_f.shape, device=post_relu_f.device)
-        acts.scatter_(dim=-1, index=post_topk.indices, src=post_topk.values)
-        return acts
+    def decode(self, top_acts, top_indices):
+        d = TritonDecoder.apply(top_indices, top_acts, self.decoder.mT)
+        return d + self.b_dec
 
-    def decode(self, f: t.Tensor) -> t.Tensor:
-        return (f @ self.decoder) + self.b_dec
-
-    def forward(self, x: t.Tensor, output_features: bool = False):
-        f = self.encode(x)
-        x_hat = self.decode(f)
-        if not output_features:
-            return x_hat
-        else:
-            return x_hat, f
+    def forward(self, x, output_features=False):
+        # (rangell): some shape hacking going on here
+        f = self.encode(x.view(-1, x.shape[-1]))
+        top_acts, top_indices = f.topk(self.k, sorted=False)
+        x_hat = self.decode(top_acts, top_indices).view(x.shape)
+        f = f.view(*x.shape[:-1], f.shape[-1])
 
     @t.no_grad()
     def set_decoder_norm_to_unit_norm(self):
@@ -119,26 +125,28 @@ class AutoEncoderTopK(Dictionary, nn.Module):
         if device is not None:
             autoencoder.to(device)
         return autoencoder
-    
+
 
 class TrainerTopK(SAETrainer):
     """
     Top-K SAE training scheme.
     """
-    def __init__(self,
-                 dict_class=AutoEncoderTopK,
-                 activation_dim=512,
-                 dict_size=64*512,
-                 k=100,
-                 auxk_alpha=1/32,  # see Appendix A.2
-                 decay_start=24000, # when does the lr decay start
-                 steps=30000, # when when does training end
-                 seed=None,
-                 device=None,
-                 layer=None,
-                 lm_name=None,
-                 wandb_name='AutoEncoderTopK',
-                 submodule_name=None,
+
+    def __init__(
+        self,
+        dict_class=AutoEncoderTopK,
+        activation_dim=512,
+        dict_size=64 * 512,
+        k=100,
+        auxk_alpha=1 / 32,  # see Appendix A.2
+        decay_start=24000,  # when does the lr decay start
+        steps=30000,  # when when does training end
+        seed=None,
+        device=None,
+        layer=None,
+        lm_name=None,
+        wandb_name="AutoEncoderTopK",
+        submodule_name=None,
     ):
         super().__init__(seed)
 
@@ -157,56 +165,57 @@ class TrainerTopK(SAETrainer):
         # Initialise autoencoder
         self.ae = dict_class(activation_dim, dict_size, k)
         if device is None:
-            self.device = 'cuda' if t.cuda.is_available() else 'cpu'
+            self.device = "cuda" if t.cuda.is_available() else "cpu"
         else:
             self.device = device
         self.ae.to(self.device)
-        
+
         # Auto-select LR using 1 / sqrt(d) scaling law from Figure 3 of the paper
-        scale = dict_size / (2 ** 14)
-        self.lr = 2e-4 / scale ** 0.5
+        scale = dict_size / (2**14)
+        self.lr = 2e-4 / scale**0.5
         self.auxk_alpha = auxk_alpha
         self.dead_feature_threshold = 10_000_000
-        
+
         # Optimizer and scheduler
         self.optimizer = t.optim.Adam(self.ae.parameters(), lr=self.lr, betas=(0.9, 0.999))
+
         def lr_fn(step):
             if step < decay_start:
-                return 1.
+                return 1.0
             else:
                 return (steps - step) / (steps - decay_start)
+
         self.scheduler = t.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lr_fn)
-    
+
         # Training parameters
         self.num_tokens_since_fired = t.zeros(dict_size, dtype=t.long, device=device)
-        
+
         # Log the effective L0, i.e. number of features actually used, which should a constant value (K)
         # Note: The standard L0 is essentially a measure of dead features for Top-K SAEs)
         self.logging_parameters = ["effective_l0", "dead_features"]
         self.effective_l0 = -1
         self.dead_features = -1
-        
+
     def loss(self, x, step=None, logging=False):
-        
         # Run the SAE
         f = self.ae.encode(x)
         top_acts, top_indices = f.topk(self.k, sorted=False)
         x_hat = self.ae.decode(top_acts, top_indices)
-        
+
         # Measure goodness of reconstruction
         e = x_hat - x
         total_variance = (x - x.mean(0)).pow(2).sum(0)
-        
+
         # Update the effective L0 (again, should just be K)
         self.effective_l0 = top_acts.size(1)
-        
+
         # Update "number of tokens since fired" for each features
         num_tokens_in_step = x.size(0)
         did_fire = t.zeros_like(self.num_tokens_since_fired, dtype=t.bool)
         did_fire[top_indices.flatten()] = True
         self.num_tokens_since_fired += num_tokens_in_step
         self.num_tokens_since_fired[did_fire] = 0
-        
+
         # Compute dead feature mask based on "number of tokens since fired"
         dead_mask = (
             self.num_tokens_since_fired > self.dead_feature_threshold
@@ -214,10 +223,9 @@ class TrainerTopK(SAETrainer):
             else None
         ).to(f.device)
         self.dead_features = int(dead_mask.sum())
-        
+
         # If dead features: Second decoder pass for AuxK loss
         if dead_mask is not None and (num_dead := int(dead_mask.sum())) > 0:
-            
             # Heuristic from Appendix B.1 in the paper
             k_aux = x.shape[-1] // 2
 
@@ -234,7 +242,7 @@ class TrainerTopK(SAETrainer):
             # Encourage the top ~50% of dead latents to predict the residual of the
             # top k living latents
             e_hat = self.ae.decode(auxk_acts, auxk_indices)
-            auxk_loss = (e_hat - e).pow(2) #.sum(0)
+            auxk_loss = (e_hat - e).pow(2)  # .sum(0)
             auxk_loss = scale * t.mean(auxk_loss / total_variance)
         else:
             auxk_loss = x_hat.new_tensor(0.0)
@@ -242,29 +250,26 @@ class TrainerTopK(SAETrainer):
         l2_loss = e.pow(2).sum(dim=-1).mean()
         auxk_loss = auxk_loss.sum(dim=-1).mean()
         loss = l2_loss + self.auxk_alpha * auxk_loss
-        
+
         if not logging:
             return loss
         else:
-            return namedtuple('LossLog', ['x', 'x_hat', 'f', 'losses'])(
-                x, x_hat, f,
-                {
-                    'l2_loss': l2_loss.item(),
-                    'auxk_loss': auxk_loss.item(),
-                    'loss' : loss.item()
-                }
+            return namedtuple("LossLog", ["x", "x_hat", "f", "losses"])(
+                x,
+                x_hat,
+                f,
+                {"l2_loss": l2_loss.item(), "auxk_loss": auxk_loss.item(), "loss": loss.item()},
             )
 
     def update(self, step, x):
-        
         # Initialise the decoder bias
         if step == 0:
             median = geometric_median(x)
             self.ae.b_dec.data = median
-            
+
         # Make sure the decoder is still unit-norm
         self.ae.set_decoder_norm_to_unit_norm()
-        
+
         # compute the loss
         x = x.to(self.device)
         loss = self.loss(x, step=step)
@@ -283,17 +288,17 @@ class TrainerTopK(SAETrainer):
     @property
     def config(self):
         return {
-            'trainer_class' : 'TrainerTopK',
-            'dict_class' : 'AutoEncoderTopK',
-            'lr' : self.lr,
-            'steps' : self.steps,
-            'seed' : self.seed,
-            'activation_dim' : self.ae.activation_dim,
-            'dict_size' : self.ae.dict_size,
-            'k': self.ae.k,
-            'device' : self.device,
-            "layer" : self.layer,
-            'lm_name' : self.lm_name,
-            'wandb_name' : self.wandb_name,
-            'submodule_name' : self.submodule_name,
+            "trainer_class": "TrainerTopK",
+            "dict_class": "AutoEncoderTopK",
+            "lr": self.lr,
+            "steps": self.steps,
+            "seed": self.seed,
+            "activation_dim": self.ae.activation_dim,
+            "dict_size": self.ae.dict_size,
+            "k": self.ae.k,
+            "device": self.device,
+            "layer": self.layer,
+            "lm_name": self.lm_name,
+            "wandb_name": self.wandb_name,
+            "submodule_name": self.submodule_name,
         }
